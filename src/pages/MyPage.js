82339651ed4d1d0b5f91d8204e9971eadd0dc8c8
qhwkal1 <qhwkal1@naver.com>
2023-06-16 21:54:27 +0900

    myPage 시작

---
+++ src/pages/MyPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import StandardStyles from "../components/StandardStyles"
+
+
+
+
+const MyPage = () => {
+    return(
+        <></>
+
+    )
+};
+
+export default MyPage;
