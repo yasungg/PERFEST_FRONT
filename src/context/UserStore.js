554b5b23f1b0c55b99112982f40cedd0b17a040f
DH <lovein6114@nate.com>
2023-06-21 20:03:45 +0900

    카카오페이 결제 구현중

--- src/context/UserStore.js
+++ src/context/UserStore.js
@@ -2,10 +2,10 @@ import { createContext, useState } from "react";
 export const UserContext = createContext(null);
 
 const UserStore = (props) => {
-  const [title, setTitle] = useState("");
-  const [value, setValue] = useState("");
-  const [total, setTotal] = useState("");
-  const [tax, setTax] = useState("");
+  const [title, setTitle] = useState("test")
+  const [value, setValue] = useState(2000);
+  const [total, setTotal] = useState(2000);
+  const [tax, setTax] = useState(0);
 
   
   return(
