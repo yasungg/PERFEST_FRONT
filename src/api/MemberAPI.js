cccfd3cf032a735bd6d1d4fe54f80281d4580ec6
qhwkal1 <qhwkal1@naver.com>
2023-06-26 15:44:37 +0900

    memberApi 수정

--- src/api/MemberAPI.js
+++ src/api/MemberAPI.js
@@ -5,11 +5,22 @@ const KH_DOMAIN = "http://localhost:8111";
 const MemberAPI = {
     // 이메일로 특정회원 조회
     MemberInfo: async(email) => {
-        return await axios.get(KH_DOMAIN + `/member/email?email=${email}`)
+        return await axios.get(KH_DOMAIN + `/member/email?email=${email}`);
     },
     
-    // 회원 탈퇴 (member status N -> Y)
-    
+    // 회원 닉네임 수정
+    updateNicName: async(id, nicName) => {
+        const updateNicName = {
+            id: id,
+            nicName: nicName
+        };
+        return await axios.post(KH_DOMAIN + "/updateNicName", updateNicName);
+    },
+    // 닉네임 수정 중복값 체크
+    nickNameRegCheck: async(nicName) => {
+        return await axios.get(KH_DOMAIN + `/nicNameCheck?nicName=${nicName}`);
+    },
+
     
 
 }
