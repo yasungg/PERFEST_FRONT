235bc6a50ac76dd66053eade91f929afdd008f4d
qhwkal1 <qhwkal1@naver.com>
2023-06-28 14:54:44 +0900

    0628 사이드바 수정

--- src/pages/Sidebar.js
+++ src/pages/Sidebar.js
@@ -1,29 +1,69 @@
-// import React from "react";
-// import { Link, NavLink } from "react-router-dom";
-// // 테스트용...
-// const SideBar = () => {
+import React from "react";
+import styled from "styled-components";
+import { SidebarData } from "./SidebarData";
 
-//     const activeStyle = {
-//         color: '#289551',
-//         fontWeight: 700,
-//     };
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
 
-//     return(
-//         <div>
-//             <div>마이페이지 테스트</div>
-//             <ul>
-//                 <li>
-//                     <NavLink style={({ isActive }) => (isActive ? activeStyle : {})} to='/MySetting'>
-//                         내 정보
-//                     </NavLink> 
-//                 </li>
-//                 <li>
-//                     <Link to='./MyPayList'>주문 내역</Link>
-//                 </li>
-//             </ul>
+const SidebarContainer = styled.div`
+  height: 100%;
+  width: 300px;
+  background-color: #2f4050;
+`;
 
-//         </div>
-//     )
-// };
+const SidebarList = styled.ul`
+  height: auto;
+  padding: 0;
+  width: 100%;
+`;
 
-// export default SideBar;
+const SidebarItem = styled.li`
+  width: 100%;
+  height: 50px;
+  border: 1px solid black;
+  list-style-type: none;
+  margin: 0%;
+  display: flex;
+  flex-direction: row;
+  color: white;
+  justify-content: center;
+  align-items: center;
+  font-family: Arial, Helvetica, sans-serif;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #293846;
+  }
+
+  &.active {
+    background-color: #293846;
+  }
+`;
+
+function SideBar() {
+  return (
+    <AppContainer>
+      <SidebarContainer>
+        <SidebarList>
+          {SidebarData.map((val, key) => {
+            return (
+              <SidebarItem
+                key={key}
+                className={window.location.pathname === val.link ? "active" : ""}
+                onClick={() => {
+                  window.location.pathname = val.link;
+                }}
+              >
+                <div id="title">{val.title}</div>
+              </SidebarItem>
+            );
+          })}
+        </SidebarList>
+      </SidebarContainer>
+    </AppContainer>
+  );
+}
+
+export default SideBar;
